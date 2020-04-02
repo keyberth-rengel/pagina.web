b663dfc9bcf2f7d24757c1094f84564eb77ae9ec
Keyberth Rengel <keyrengel24@gmail.com>
2020-04-02 16:09:33 +0100

    push

--- src/Componentes/Carrito/Carrito.js
+++ src/Componentes/Carrito/Carrito.js
@@ -1,39 +1,55 @@
-import React, {Component} from 'react';
-import ProductoCarrito from '../ProductosCarrito/ProductoCarrito';
-import './Carrito.css';
+import React, { Component } from "react";
+import ProductoCarrito from "../ProductosCarrito/ProductoCarrito";
+import "./Carrito.css";
 
 class Carrito extends Component {
-    render() {
-        const productoscarrito = this.props.carrito;
-        return (
-            <div className="row mt-2 mb-5 pb-3">
-                <div className="carrito">
-                    <p className="enca-carrito">Tu TiendaOnline donde encuentras ofertas <img src="./img/logo3.jpg" alt="logo" /> </p>
-                    <h2>Tus Productos por comprar</h2>
+  render() {
+    const productoscarrito = this.props.carrito;
+    return (
+      <div className="row mt-5 mb-5 pb-3">
+        <div className="carrito pt-5">
+          <p className="enca-carrito">
+            Tu TiendaOnline donde encuentras ofertas{" "}
+            <img src="./img/logo3.jpg" alt="logo" />{" "}
+          </p>
+          <h2>Tus Productos Por Comprar</h2>
 
-                    <div className="col-10 col-md-8 tabla justify-content-center">
-                        <ul className="">
-                            <li className="">
-                                <p className="titulo m-0">Producto</p>
-                            </li>
-                            <li className="">
-                                <p className="titulo m-0">Cantida</p>
-                            </li>
-                            <li className="">
-                                <p className="titulo m-0">Precio</p>
-                            </li>
-                            <li className="">
-                                <p className="titulo m-0">Opciones</p>
-                            </li>
-                        </ul>
-                    </div>
-                    {
-                        
-                        Object.keys(productoscarrito).length === 0 ? <div className="col-10 col-md-8 tabla"> <p className="vacio text-center alert alert-danger">Su carrito esta vacio. </p> </div> : Object.keys(productoscarrito).map( producto => ( <ProductoCarrito key={producto} productocarrito={productoscarrito[producto]} borrarPedido={this.props.borrarPedido}comprarProducto={this.props.comprarProducto} /> ) )
-                    }
-                </div>
+          <div className="col-10 col-md-8 tabla justify-content-center">
+            <ul className="">
+              <li className="">
+                <p className="titulo m-0">Producto</p>
+              </li>
+              <li className="">
+                <p className="titulo m-0">Cantidad</p>
+              </li>
+              <li className="">
+                <p className="titulo m-0">Precio</p>
+              </li>
+              <li className="">
+                <p className="titulo m-0">Opciones</p>
+              </li>
+            </ul>
+          </div>
+          {Object.keys(productoscarrito).length === 0 ? (
+            <div className="col-10 col-md-8 tabla">
+              {" "}
+              <p className="vacio text-center alert alert-danger">
+                Su carrito esta vacio.{" "}
+              </p>{" "}
             </div>
-        )
-    }
+          ) : (
+            Object.keys(productoscarrito).map(producto => (
+              <ProductoCarrito
+                key={producto}
+                productocarrito={productoscarrito[producto]}
+                borrarPedido={this.props.borrarPedido}
+                comprarProducto={this.props.comprarProducto}
+              />
+            ))
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 export default Carrito;
--- src/Componentes/Usuario/Usuario.js
+++ src/Componentes/Usuario/Usuario.js
@@ -1,49 +1,76 @@
-import React, {Component} from 'react';
-import './Usuario.css';
+import React, { Component } from "react";
+import "./Usuario.css";
 
-class Usuario extends Component{
+class Usuario extends Component {
+  state = {
+    error: false
+  };
 
-    state = {
+  // emailRef = React.createRef();
+  // passRef = React.createRef();
+
+  ingresar = e => {
+    e.preventDefault();
+    // const email = this.emailRef.current.value,
+    //       pass = this.passRef.current.value;
+    // if(email === "" || pass === "" ) {
+    this.setState({
+      error: true
+    });
+    // }
+
+    setTimeout(() => {
+      this.setState({
         error: false
-    }
+      });
+    }, 4000);
+  };
 
-    // emailRef = React.createRef();
-    // passRef = React.createRef();
-    
-    ingresar = (e) => {
-        e.preventDefault();
-        // const email = this.emailRef.current.value,
-        //       pass = this.passRef.current.value;
-        // if(email === "" || pass === "" ) {
-            this.setState({
-                error: true
-            });
-        // }
+  render() {
+    const error = this.state.error;
 
-        setTimeout(() => {
-            this.setState({
-                error: false
-            });
-        }, 4000)
-    }
+    const fallo = error ? (
+      <div className="btn btn-danger my-2 py-3 text-white text-center">
+        Usuario o Contraseña incorrectos. Intente nuevamente.
+      </div>
+    ) : (
+      ""
+    );
 
-    render() {
-        const error = this.state.error;
+    return (
+      <div
+        className="iniciar-sesion my-4 col-ms-12 col-md-8 col-lg-8"
+        id="iniciar"
+      >
+        <p className="enca1-form">Iniciar Seccion</p>
+        <form
+          className="pb-5 formulario"
+          onSubmit={this.ingresar}
+          id="iniciar-form"
+          name="iniciar-form"
+        >
+          <input
+            type="email"
+            ref={this.emailRef}
+            className="texto-formulario"
+            name="correo"
+            id="correo-iniciar"
+            placeholder="Correo electronico"
+          />
 
-        const fallo = error ? <div className="btn btn-danger my-2 py-3 text-white text-center">Usuario o Contraseña incorrectos. Intente nuevamente.</div> : "";
-
-        return(
-            <div className="iniciar-sesion my-4" id="iniciar">
-                <p className="enca1-form">Iniciar Seccion</p>
-                <form className="pb-5 formulario" onSubmit={this.ingresar} id="iniciar-form" name="iniciar-form">
-                    <input type="email" ref={this.emailRef} className="texto-formulario" name="correo" id="correo-iniciar" placeholder="Correo electronico" />
-
-                    <input type="password" ref={this.passRef} className="texto-formulario" name="password" id="password-iniciar" placeholder="Contraseña" />
-                    {fallo}
-                    <input type="submit" name="boton" value="Iniciar" id="btn-iniciar" />
-                </form>
-            </div>
-        )
-    }
+          <input
+            type="password"
+            ref={this.passRef}
+            className="texto-formulario"
+            name="password"
+            id="password-iniciar"
+            placeholder="Contraseña"
+          />
+          {fallo}
+          <input type="submit" name="boton" value="Iniciar" id="btn-iniciar" />
+        </form>
+      </div>
+    );
+  }
 }
 export default Usuario;
